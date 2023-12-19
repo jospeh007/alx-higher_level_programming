defd840b063a904050830293e3219583efe71796
jospeh007 <youssefgf123@gmail.com>
2023-12-19 14:34:04 -0800

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Display the square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new a square module.
+        Args:
+            size integer: The size of the new square module.
+            position (int, int): The position of the new square module.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the current position of the square."""
+        return (self.__position)
+
+    @size.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square module."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square module with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
