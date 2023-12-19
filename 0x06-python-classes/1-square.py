dba2ee49d3af4690ba847a7717aea332877022a3
jospeh007 <youssefgf123@gmail.com>
2023-12-19 13:07:03 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines an empty square."""
+
+    def __init__(self, size):
+        """More important.
+
+        Args:
+            size: Length of a size of the square module.
+        """
+        self.__size = size
