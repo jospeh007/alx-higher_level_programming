3e43f1f0f3ab65f85583aa53365825e4d710d971
jospeh007 <youssefgf123@gmail.com>
2023-12-19 13:13:32 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines an empty square."""
+
+    def __init__(self, size=0):
+        """More important.
+
+        Args:
+            size: Length of a size of the square.
+
+        Exception:
+            TypeError: If the size is not an int
+            ValueError: If the size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
