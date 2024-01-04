6c4a708cb5555bbb92bc1889201448e819096dea
jospeh007 <youssefgf123@gmail.com>
2024-01-04 14:24:46 -0800

    ff

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,10 +7,7 @@ Display a Rectangle
 class Rectangle:
     """Display of a Rectangle"""
     def __init__(self, width=0, height=0):
-
         """Initializes of the rectangle"""
-        self.width = width
-        self.height = height
 
     @property
     def width(self):
