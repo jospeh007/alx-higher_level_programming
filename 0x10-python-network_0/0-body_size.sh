82a208dc9c2045ab78eb731206a27fe38d4c1606
jospeh007 <youssefgf123@gmail.com>
2024-03-27 19:34:09 -0700

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,3 @@
-#!/usr/bin/python3
-
-# Check if URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Assign URL from command-line argument
-URL=$1
-
-# Send request using curl and capture response body size
-response=$(curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}')
-
-# Display the size of the response body in bytes
-if [ -z "$response" ]; then
-    echo "Failed to retrieve content size. Please check if the URL is correct."
-else
-    echo "Response body size: $response bytes"
-fi
+#!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
